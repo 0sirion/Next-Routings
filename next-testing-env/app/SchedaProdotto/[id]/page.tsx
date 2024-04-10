4f79531d151fb838f0dbac006f9f7b33bf089f36
0sirion <salvatore.arcuri.93@gmail.com>
2024-04-10 15:44:35 +0200

    secondo commit

--- next-testing-env/app/SchedaProdotto/[id]/page.tsx
+++ next-testing-env/app/SchedaProdotto/[id]/page.tsx
@@ -4,55 +4,95 @@ import Link from "next/link";
 import Image from "next/image";
 import { useState, useEffect } from "react";
 
-const DettagliProdotto = ({ id }: { id: string }) => {
-  console.log("Questo è l'id del prodotto: ", id);
+const DettagliProdotto = ({ params }: { params: { id: string } }) => {
+  console.log("Questo è l'id del prodotto: ", params.id);
 
   const [prodotto, setProdotto] = useState<any>(null);
+  const idString = params.id.toString();
 
   useEffect(() => {
     const fetchProdotto = async () => {
       try {
-        const response = await fetch(`https://dummyjson.com/products/${id}`);
+        const response = await fetch(
+          `https://dummyjson.com/products/${idString}`
+        );
         const data = await response.json();
         setProdotto(data);
-        console.log(data);
+        console.log("Prodotto Fetchato", data);
       } catch (error) {
         console.error("Errore nel fetch dei dati:", error);
       }
     };
 
     fetchProdotto();
-  }, [id]);
-
-  if (!prodotto) {
-    return <Typography variant="h5">Caricamento...</Typography>;
-  }
+  }, []);
 
   return (
     <Box sx={{ margin: 10 }}>
-      <Typography variant="h5" sx={{ color: "blue" }}>
-        {prodotto.title}
+      <Typography variant="h3">
+        {prodotto ? prodotto.title : "Caricamento in corso..."}
       </Typography>
-
-      <Typography variant="body1" sx={{ color: "black" }}>
-        {prodotto.description}
+      {prodotto && prodotto.images && prodotto.images.length > 0 && (
+        <Image
+          src={prodotto.images[0]} // Utilizza il primo elemento dell'array images
+          alt="Immagine del prodotto"
+          width={300}
+          height={300}
+        />
+      )}
+      <Typography variant="body1">
+        {prodotto ? prodotto.description : ""}
       </Typography>
-      <Typography variant="h6" sx={{ color: "red" }}>
-        Prezzo: {prodotto.price} Euro
+      <Typography variant="body1">
+        {prodotto ? `Prezzo: ${prodotto.price}` : ""}
       </Typography>
-      <Link href="/Shop">
-        <Button variant="contained">Torna allo Shop</Button>
-      </Link>
-      <Button
-        variant="contained"
-        onClick={() => {
-          console.log("Questo è l'id del prodotto: ", id);
-        }}
-      >
-        id
-      </Button>
     </Box>
   );
 };
 
 export default DettagliProdotto;
+
+// "use client";
+// import { Box, Button, Typography } from "@mui/material";
+// import Link from "next/link";
+// import Image from "next/image";
+// import { useState, useEffect } from "react";
+
+// const DettagliProdotto = ({ params }: { parms: { id: string } }) => {
+//   console.log("Questo è l'id del prodotto: ", params.id);
+
+//   const [prodotto, setProdotto] = useState<any>(null);
+//   const idString = params.id.toString();
+
+//   useEffect(() => {
+//     const fetchProdotto = async () => {
+//       try {
+//         const response = await fetch(
+//           `https://dummyjson.com/products/${idString}`
+//         );
+//         const data = await response.json();
+//         setProdotto(data);
+//         console.log("Prodotto Fetchato con use", prodotto);
+//       } catch (error) {
+//         console.error("Errore nel fetch dei dati:", error);
+//       }
+//     };
+
+//     fetchProdotto();
+//   }, []);
+
+//   return (
+//     <Box sx={{ margin: 10 }}>
+//       <Typography></Typography>
+//       <Button
+//         onClick={() => {
+//           console.log(prodotto);
+//         }}
+//       >
+//         Prodotto
+//       </Button>
+//     </Box>
+//   );
+// };
+
+// export default DettagliProdotto;
---
+++ next-testing-env/app/Shop/[idProdotto]/page.tsx
@@ -0,0 +1,7 @@
+export default function DettagliProdotto({ params }) {
+  return (
+    <div>
+      <h1>Dettagli Prodotto {params.idProdotto}</h1>
+    </div>
+  );
+}
